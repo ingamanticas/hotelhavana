34197400a6a32777b42a50d73141e3e954ac8544
ingamanticas <54080178+ingamanticas@users.noreply.github.com>
2019-12-10 13:55:28 -0500

    Delete main.js

--- FINAL HOTEL WEBSITE CODE/main.js
+++
@@ -1,6 +0,0 @@
-$( ".cta" ).click(function() {
-  $( ".transition").toggleClass( "anim-trans" );
-  $(".transition2").toggleClass("ranim-trans");
-  $('.transition').fadeIn();
-  $('.transition2').fadeIn();
-});
